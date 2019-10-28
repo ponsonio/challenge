7764efaa0287abc1e0b59246dd86da416eceaf27
ponsonio@gmail.com <jcabrera@Jorges-MacBook-Pro.local>
2019-10-28 15:44:09 -0500

    Add check in and out tests

--- employee_checkin/test/controllers/check_controller_test.rb
+++ employee_checkin/test/controllers/check_controller_test.rb
@@ -25,4 +25,28 @@ class CheckControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should check id" do
+    get check_index_url + '?employee_id=' + @employee.id.to_s
+    assert_response :success
+  end
+
+  test "should check in" do
+    start = @employee.assistances.count
+    get check_in_url + '?employee_id=' + @employee.id.to_s
+    assert_response :success
+    assert_equal(start + 1, @employee.assistances.count, 'should have only one more assistance')
+    last_assistance = @employee.assistances.order(created_at: :asc).last(1).first
+    assert_nil(last_assistance.checkout, 'checkout date should be null')
+  end
+
+  test "should check out" do
+    get check_in_url + '?employee_id=' + @employee.id.to_s
+    get check_out_url + '?employee_id=' + @employee.id.to_s
+    assert_response :success
+    @employee = Employee.find(@employee.id)
+    last_assistance = @employee.assistances.order(created_at: :asc).last(1).first
+    assert_not_nil(last_assistance.checkout, 'checkout date should be not null')
+  end
+
+
 end
